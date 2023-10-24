72f995619827e70086face197eb5f67cf03e83f4
gatino <gatinocorp@gmail.com>
2023-10-24 09:44:23 -0300

    ...

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,26 +1,22 @@
-import { KeyboardEvent, useState } from "react"
+import { FocusEvent, KeyboardEvent, useState } from "react"
 import './App.css'
 
 export default function App() {
+  const [currentItem, setCurrentItem] = useState<number | null>(null)
   const [todolist, setTodolist] = useState<string[]>(
     JSON.parse(localStorage.getItem('todolist') ?? '[]')
   )
+
 
   const onKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       const value = event.currentTarget.value
       event.currentTarget.value = ''
-      const newTodolist = [...todolist, value]
+      const newTodolist = [value, ...todolist]
       setTodolist(newTodolist)
       localStorage.setItem('todolist', JSON.stringify(newTodolist))
     }
   }
-
-  // const removeItem = (key: number) => {
-  //   const newTodolist = [...todolist]
-  //   newTodolist.splice(key, 1)
-  //   setTodolist(newTodolist)
-  // }
 
   const removeItem = (index: number) => {
     const newTodolist = todolist.filter((_, key) => key !== index)
@@ -28,18 +24,38 @@ export default function App() {
     localStorage.setItem('todolist', JSON.stringify(newTodolist))
   }
 
+  const onUpdateFocus = (event: FocusEvent<HTMLInputElement, Element>) => {
+    if (currentItem)
+      event.currentTarget.value = todolist[currentItem]
+  }
+
+  const updateInputFn = (event: KeyboardEvent<HTMLInputElement>, key: number) => {
+    if (event.key === 'Enter') {
+      const value = event.currentTarget.value
+      const newTodolist = todolist.map((todo, index) => index === key ? value : todo)
+      console.log(newTodolist)
+      setTodolist(newTodolist)
+      localStorage.setItem('todolist', JSON.stringify(newTodolist))
+    }
+  }
+
   return <>
     <h1>Lista de tarefas</h1>
-    <input type="text" onKeyDown={onKeyDown} />
+    <input className="input-action" type="text" onKeyDown={onKeyDown} />
     <ul>
-      <li className="pending">pending</li>
-      <li className="synced">synced</li>
-      <li className="error">error</li>
+      {/* <li className="pending">pending</li> */}
+      {/* <li className="synced">synced</li> */}
+      {/* <li className="error">error</li> */}
       {todolist.map((todo, key) =>
         <li key={key} className="synced">
-          <button onClick={() => removeItem(key)}>remove</button> 
-          <button>update</button>
-          <span>{todo}</span>
+          <button onClick={() => removeItem(key)}>remove</button>
+          <button onClick={() => setCurrentItem(key)}>update</button>
+          {key === currentItem
+            ? <input
+              data-id={key}
+              onFocus={onUpdateFocus}
+              onKeyDown={e => updateInputFn(e, key)} />
+            : <span>{todo}</span>}
         </li>
       )}
     </ul>
