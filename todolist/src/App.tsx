357e1a207683cc259b2266ef52c9b7ccca3aca59
gatino <gatinocorp@gmail.com>
2023-10-24 08:42:54 -0300

    ...

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,24 +1,45 @@
 import { KeyboardEvent, useState } from "react"
+import './App.css'
 
 export default function App() {
-  const [todolist, setTodolist] = useState([ 'Fazer café', 'Estudar React', 
-  'Ir para o estágio'])
+  const [todolist, setTodolist] = useState<string[]>(
+    JSON.parse(localStorage.getItem('todolist') ?? '[]')
+  )
 
   const onKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       const value = event.currentTarget.value
       event.currentTarget.value = ''
-      setTodolist([...todolist, value])
+      const newTodolist = [...todolist, value]
+      setTodolist(newTodolist)
+      localStorage.setItem('todolist', JSON.stringify(newTodolist))
     }
+  }
+
+  // const removeItem = (key: number) => {
+  //   const newTodolist = [...todolist]
+  //   newTodolist.splice(key, 1)
+  //   setTodolist(newTodolist)
+  // }
+
+  const removeItem = (index: number) => {
+    const newTodolist = todolist.filter((_, key) => key !== index)
+    setTodolist(newTodolist)
+    localStorage.setItem('todolist', JSON.stringify(newTodolist))
   }
 
   return <>
     <h1>Lista de tarefas</h1>
     <input type="text" onKeyDown={onKeyDown} />
     <ul>
+      <li className="pending">pending</li>
+      <li className="synced">synced</li>
+      <li className="error">error</li>
       {todolist.map((todo, key) =>
-        <li key={key}>
-          <button>remove</button> {todo}
+        <li key={key} className="synced">
+          <button onClick={() => removeItem(key)}>remove</button> 
+          <button>update</button>
+          <span>{todo}</span>
         </li>
       )}
     </ul>
